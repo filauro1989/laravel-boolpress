3d228d591bcac23e00d257232eb529bbd7b82f71
filauro1989 <filauro1989@gmail.com>
2022-02-28 16:31:49 +0100

    index posts completed

--- resources/views/Admin/posts/index.blade.php
+++ resources/views/Admin/posts/index.blade.php
@@ -1,1 +1,46 @@
-@dd($posts)
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <h1 class="h1">Admin - All Posts</h1>
+        </div>
+        {{-- <div class="row">
+            <div class="col">
+                <a href="{{ route('keyboards.create') }}" class="btn btn-primary">Add new keyboard</a>
+            </div>
+        </div> --}}
+        <div class="row">
+            <div class="col">
+                <table class="table table-primary">
+                    <thead>
+                        <tr class="table-primary">
+                            <th>Title</th>
+                            <th>Author</th>
+                            <th>Content</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($posts as $post)
+                        <tr>
+                            <td>{{ $post->title }}</td>
+                            <td>{{ $post->author }}</td>
+                            <td>{{ $post->content }} €</td>
+                            {{-- <td><a class="btn btn-primary" href="{{ route('posts.show', $post) }}">View</a></td> --}}
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                {{ $posts->links() }}
+            </div>
+        </div>
+    </div>
+
+@endsection
+
