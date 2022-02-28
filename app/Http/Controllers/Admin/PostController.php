d9d37a918cb1e33acde22403669c49c592e2c60e
filauro1989 <filauro1989@gmail.com>
2022-02-28 18:21:12 +0100

    controller updated

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -74,8 +74,9 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        return view('admin.posts.show', compact('post'));
+        return view('admin.posts.show', ['post' => $post]);
     }
+
 
     /**
      * Show the form for editing the specified resource.
