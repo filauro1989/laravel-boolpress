79d3dd0507b415e6049008a5e4172fe8399e5c31
filauro1989 <filauro1989@gmail.com>
2022-03-02 14:43:48 +0100

    admin created

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -15,7 +15,16 @@ class UserSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+
+        $newUser = new User();
+        $newUser->name = 'Admin';
+        $newUser->email = 'filauro@gmail.com';
+        $string = 'filauro1989';
+        $newUser->password = Hash::make($string);
+        $newUser->save();
+        
         for ($i=0; $i < 10; $i++) { 
+
             $newUser = new User();
             $newUser->name = $faker->name();
             $newUser->email = $faker->email();
