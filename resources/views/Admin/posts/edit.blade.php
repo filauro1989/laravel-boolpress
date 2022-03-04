ac9cd7cb1aa8f7eab55990899b31bfcdf780512a
filauro1989 <filauro1989@gmail.com>
2022-03-04 16:36:23 +0100

    edit.blade completed

--- resources/views/Admin/posts/edit.blade.php
+++ resources/views/Admin/posts/edit.blade.php
@@ -13,22 +13,56 @@
         </div>
         <div class="row">
             <div class="col d-flex justify-content-center">
-                <div class="card w-50 d-flex align-items-center">
+                <div class="card w-50 d-flex align-items-center py-3">
                     <form action="{{ route('admin.posts.update', $post->slug) }}" method="post">
                         {{-- essenziali per il funzionamento del form --}}
                         @csrf
                         @method('PATCH')
 
+                        <legend>Categories</legend>
                         <select class="form-select" name="category_id">
                             <option value="">Select a category</option>
                             @foreach ($categories as $category)
-                                <option @if (old('category_id') == $category->id) selected @endif value="{{ $category->id }}">
+                                <option @if (old('category_id') == $category->id) selected 
+                                    @endif value="{{ $category->id }}">
                                     {{ $category->name }}</option>
                             @endforeach
                         </select>
+                        @error('category_id')
+                        <div class="alert alert-danger mt-3">
+                            {{ $message }}
+                        </div>
+                        @enderror
+
+                        <fieldset class="my-3">
+                            <legend>Tags</legend>
+                            {{-- se abbiamo gia compilato il form e siamo tornati indietro per validazione errata allora facciamo un foreach e controlliamo old('tags') --}}
+                            @if ($errors->any())
+                                @foreach ($tags as $tag)
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="{{ $tag->id }}" name="tags[]"
+                                            {{ in_array($tag->id, old('tags', [])) ? 'checked' : '' }}>
+                                        <label class="form-check-label" for="flexCheckDefault">
+                                            {{ $tag->name }}
+                                        </label>
+                                    </div>
+                                @endforeach
+                            @else
+                                {{-- Altrimenti prendiamo i dati dal db e checchiamo i nostri checkbox corrispondenti --}}
+                                @foreach ($tags as $tag)
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="{{ $tag->id }}" name="tags[]"
+                                            {{ $post->tags()->get()->contains($tag->id) ? 'checked': '' }}>
+                                        <label class="form-check-label" for="flexCheckDefault">
+                                            {{ $tag->name }}
+                                        </label>
+                                    </div>
+                                @endforeach
+                            @endif
+                        </fieldset>
 
                         <div class="mb-3">
-                            <label for="title" class="form-label">title</label>
+                            <legend>Title</legend>
                             <input type="text" class="form-control" id="title" name="title" value="{{ $post->title }}">
     
                             @error('title')
@@ -37,7 +71,7 @@
                         </div>
 
                         <div class="mb-3">
-                            <label for="author" class="form-label">author</label>
+                            <legend>Author</legend>
                             <input type="text" class="form-control" id="author" name="author" value="{{ $post->author }}">
     
                             @error('author')
@@ -46,7 +80,7 @@
                         </div>
 
                         <div class="mb-3">
-                            <label for="content" class="form-label">content</label>
+                            <legend>Content</legend>
                             <input type="text" class="form-control" id="content" name="content" value="{{ $post->content }}">
     
                             @error('content')
