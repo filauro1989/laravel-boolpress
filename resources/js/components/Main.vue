673d11f662c6744e4c743fa9ccbe2e342860608b
filauro1989 <filauro1989@gmail.com>
2022-03-08 16:35:12 +0100

    main compiled

--- resources/js/components/Main.vue
+++ resources/js/components/Main.vue
@@ -1,8 +1,51 @@
 <template>
-    <div>Hello World</div>
+    <div>
+        <div class="container">
+            <div class="row row-cols-1 row-cols-md-4 g-4">
+                <div class="col" v-for="(post, index) in posts" :key="index">
+                    <div class="card">
+                        <img
+                            :src="'/storage/' + post.image"
+                            class="card-img-top"
+                            :alt="post.title"
+                        />
+                        <div class="card-body">
+                            <h3 class="card-title">{{ post.author }}</h3>
+                            <h5 class="card-title">{{ post.title }}</h5>
+                            <p class="card-text">{{ post.content }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-3 bg-light">
+                <ul class="list-inline bg-light">
+                    <li class="list-inline-item">
+                        <button
+                            v-if="prev_page_url"
+                            class="btn btn-primary"
+                            @click="changePage('prev_page_url')"
+                        >
+                            Prev
+                        </button>
+                    </li>
+                    <li class="list-inline-item">
+                        <button
+                            v-if="next_page_url"
+                            class="btn btn-primary"
+                            @click="changePage('next_page_url')"
+                        >
+                            Next
+                        </button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import Axios from "axios";
+
 export default {
     name: "Main",
     data() {
